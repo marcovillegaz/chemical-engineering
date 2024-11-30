ac9eed9c24c89980dac51b48b6a40aba79ff90dc
Marco Asus Laptop <marcovillegas.g@gmail.com>
2024-11-30 19:21:36 -0300

    basic reactor added with them models

--- reaction_kinetics/reactor.py
+++ reaction_kinetics/reactor.py
@@ -3,17 +3,10 @@ import scipy.integrate as integrate
 
 
 class Reactor:
-    def __init__(
-        self,
-        reactor_type,
-        order_of_reaction,
-        kinetic_constant,
-        fractional_change=0,
-    ):
-        self.reactor_type = reactor_type
+    def __init__(self, order_of_reaction, kinetic_constant):
         self.order_of_reaction = order_of_reaction
         self.kinetic_constant = kinetic_constant
-        self.fractional_change = fractional_change
+        self.reactor_type = None  # Default to None, subclasses can set this
 
     def residence_time(self):
         # This method will be overridden by child classes
@@ -36,10 +29,15 @@ class Reactor:
 
 class PlugFlow(Reactor):
     def __init__(self, order_of_reaction, kinetic_constant):
-        super().__init__("Plug Flow", order_of_reaction, kinetic_constant)
+        super().__init__(order_of_reaction, kinetic_constant)
+        self.reactor_type = "Plug Flow"
 
     def residence_time(
-        self, output_conversion, input_concentration, input_conversion, epsilon
+        self,
+        output_conversion,
+        input_concentration,
+        input_conversion,
+        epsilon,
     ):
         # Redifinition of variable for better manipulation
         n = self.order_of_reaction
@@ -59,10 +57,15 @@ class PlugFlow(Reactor):
 
 class StirredTank(Reactor):
     def __init__(self, order_of_reaction, kinetic_constant):
-        super().__init__("Stirred Tank", order_of_reaction, kinetic_constant)
+        super().__init__(order_of_reaction, kinetic_constant)
+        self.reactor_type = "Stirred Tank"
 
     def residence_time(
-        self, output_conversion, input_concentration, input_conversion, epsilon
+        self,
+        output_conversion,
+        input_concentration,
+        input_conversion,
+        epsilon,
     ):
         # Redifinition of variable for better manipulation
         n = self.order_of_reaction
@@ -72,5 +75,42 @@ class StirredTank(Reactor):
         Xai = input_conversion
 
         return (1 / (k * Ca0 ** (n - 1))) * (
-            ((Xaf * ((1 + epsilon * Xaf) ** n) - Xai)) / ((1 - Xaf) ** n)
+            Xaf * (((1 + epsilon * Xaf) / (1 - Xaf)) ** n)
         )
+
+
+class RecyclePlugFlow(Reactor):
+    def __init__(self, order_of_reaction, kinetic_constant):
+        super().__init__(order_of_reaction, kinetic_constant)
+        self.reactor_type = "Recycle Plug Flow"
+
+    def residence_time(self, R, initial_cocnentration, final_conversion):
+        Ca0 = initial_cocnentration
+        Xa_f = final_conversion
+        Xa_i = (R / (R + 1)) * Xa_f
+        n = self.order_of_reaction
+        k = self.kinetic_constant
+
+        integral, _ = integrate.quad(
+            lambda Xa: 1 / ((k * (Ca0 * (1 - Xa)) ** (n - 1))),
+            Xa_i,
+            Xa_f,
+        )
+        tau = Ca0 * (R + 1) * integral
+        return tau
+
+    def avarage_rate(self, R, final_conversion, initial_cocnentration):
+        Ca0 = initial_cocnentration
+        Xa_f = final_conversion
+        Xa_i = (R / (R + 1)) * Xa_f
+        n = self.order_of_reaction
+        k = self.kinetic_constant
+
+        integral, _ = integrate.quad(
+            lambda Xa: 1 / ((k * (Ca0 * (1 - Xa)) ** (n - 1))),
+            Xa_i,
+            Xa_f,
+        )
+
+        av_rate = integral / (Xa_f - Xa_i)
+        return av_rate
